413502767e0fa6dad72e3f072e54436bee80e4a2
James Cook <jamesjcook@users.noreply.github.com>
2018-09-28 09:29:33 -0400

    Converted input convention to source_tree, dest_tree, branch. Updated to have failure return codes when appropriate.

--- LibManager.pl
+++ LibManager.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 # LibManager
 # tranfers library to new location using lib.conf rules to omit all data which wouldnt be displayed.
-# updates clean libraries
-# verifies library contents.
+# updates clean libraries of removed content. 
+# inherrently verifies library contents from source to dest, 
+# WARNING: It removes excess from dest, THIS IS NOT A BACKUP OR CLONE UTILITY!
 
 use strict;
 use warnings;
@@ -90,30 +91,100 @@ sub Main
     }else {
 	$opts{"c"}="";
     }
-    my $source_path=$ARGV[0];
-    my $dest_starter=$ARGV[1];
-
+    # Given a datalibrary input with index
+    # DataLibraries/MySpecialThing
+    # and a desired output library of
+    # ReducedLibraries/MySpecialThing
+    # 
+    # source tree would be DataLibraries
+    # dest tree would be ReducedLibraries
+    # and item would be MySPecialThing.
+    #
+    # dest_tree can be a subtree of source_tree,
+    # source tree can NEVER be a sub tree of dest tree BECAUSE it will be pruned. 
     my @errors;
+    my $source_tree=$ARGV[0] or push(@errors,"Missing our source tree ");
+    my $dest_tree=$ARGV[1] or  push(@errors,"Missing our dest tree");
+    my $item=$ARGV[2] or push(@errors,"Missing the item!");;
+    
+
     #    my @errors=check_inputs($source_path,$dest_starter,\%opts);
-    if (scalar (@ARGV)!=2 ){
-	push(@errors,"EXTRA ARGS! Did you put options in wrong place.");
+    ###
+    # Input checking, do we have the right number of args. 
+    if (scalar (@ARGV)!=3 ){
+	push(@errors,"WRONG ARG COUNT! Did you put options in wrong place.");
     }
     if ( $#errors>=0 ) {
 	print(join("\n",@errors)."\n");
 	return 1;
     }
+    # Link support disabled for now since we should probably readlink in that case.
+    if ( ! -d $source_tree) { # && ! -l $source_tree ) {
+	confess( "Unavailable source: $source_tree !");
+    }
+    
+    # source_path, the path to our item in source_tree.
+    my $source_path=path_collapse(File::Spec->catfile($source_tree,$item));
+    
+    # dest_path, the path to our item in dest_tree.
+    my $dest_path=path_collapse(File::Spec->catfile($dest_tree,$item));
+    # Input checking, were we given a file(like mabye a lib.conf?
+    if ( -f $source_path ) {
+        # odd file passed as our item, drop back a level. 
+        $source_path=$source_tree;
+        $dest_path  =$dest_tree;
+    } elsif( -d $source_path ) { #|| -l $source_path ) {
+        # Link support disabled for now since we should probably readlink in that case.
+    } else {
+        die "BAD INPUT";
+    }
+    
+    # Input checking, is the last element of source_path and dest_path the same? Its supposed to be.
+    my ($si,$sdir)=fileparse($source_path);
+    my ($di,$ddir)=fileparse($dest_path);
+    if ( $si ne $di ) {
+        die("Source item:$si, doesnt match dest item:$di\n");
+    }
+    
+    # Input checking, is the source a subtree of destination? That shouldn't be.
+    # We'll figure that out by removing matching parts of the two paths.
+    
+    my @s_parts = File::Spec->splitdir( $sdir );
+    my @d_parts = File::Spec->splitdir( $ddir );
+    my $matching_parts=0;
+    printd(45,"err checking: is source part of dest?(by triming front of paths)\n");
+    while(scalar(@s_parts) > 0 && 
+          scalar(@d_parts) > 0 ) {
+        if ( $s_parts[0] eq $d_parts[0] ) {
+            my $t=shift(@s_parts);
+            shift(@d_parts);
+            $matching_parts++;
+            printd(45,"\t/$t\n");
+        } else {
+            last;
+        }
+    }
+    if ( $matching_parts>0 && scalar(@d_parts) == 0 ) {#scalar(@s_parts) > scalar(@d_parts) &&
+        die "source cannot be part of destingation! Destination must have at least the final level path unique.";
+    } else {
+        print("there are $matching_parts common parts to the path\n");
+    }
+    print("source_path:\t$source_path\n");
+    #my $dest_path=get_full_destpath($source_path,$dest_starter);
 
     # what are the steps to transfer and update libs?
     # figure out where in the path we want to do stuff.
     #  eg, how much of the path is symetric, once the path is symetric stop, run through the mkdir bits.
     # In plain language, parse dest_starter, and get last component dir.
     # Now we find that component in source, the components up to that point is the source_base, which is allowed to fluctuate.
+    #
+    # THIS MAKES THE CODE SO HARD TO USE! STop that!!!
+    # converiing inputs to be , source_tree, dest_tree, common_item
+    # that will transver commn_item in source_tree to dest_tree, 
 
-    #my ($source_trail,$dest_base)=get_source_trail($source_path,$dest_starter);
-
-    print("source_path:\t$source_path\n");
-    print("dest_init:\t$dest_starter\n");
-    my $dest_path=get_full_destpath($source_path,$dest_starter);
+    ###
+    # If source path is file, cut that to its directory.
+    # Moving this up to happen at the same time these are created!.
     if (-f $source_path ){
 	my $fn;
 	($fn,$source_path)=fileparse($source_path);
@@ -123,21 +194,20 @@ sub Main
     if ( ! -d $source_path ) {
 	confess( "Unavailable source: $source_path !");
     }
-    die;
+
     my %dir_opts=('chmod' => 0777);
     if ( ! -d $dest_path ) {
 	print("\tmkdir -p $dest_path\n");
 	make_path($dest_path,\%dir_opts) if $debug_val<50;
     }
     #$dest_base/$source
-    # we want mkdir $dest_base/$source_trail
     
     #my $failures = manage_lib($source_path,$dest_path);
-
     my $work=lib_parse($source_path,$dest_path,$opts{c});
     if ($can_dump){
 	Data::Dump::dump($work);
     }
+        
     my $failures=do_work($source_path,$dest_path,$work);
     if ($#{$failures} >= 0 ){
 	print ("Fail manage:\n".join("\n",@{$failures})."\n");
@@ -192,6 +262,8 @@ sub check_inputs
     
 sub get_full_destpath
 {
+    funct_obsolete("get_full_destpath","too complicated makes a mess, how about you die insted?");
+    die;
     my ($source,$dest)=@_;
     my @s_parts=File::Spec->splitdir(path_collapse($source));
     my @d_parts=File::Spec->splitdir(path_collapse($dest));
@@ -213,6 +285,7 @@ sub get_full_destpath
     
     path_collapse(path)
     takes a path in and collapses any ../ elements in the middle of the path to their correct compenent.
+    also remove bonus slashes becuase it uses splitdir
     ex, /thing/thing2/../thinga = /thing/thinga
     ex2, /thing/thing2/thio/../../ = /thing
 
@@ -224,12 +297,24 @@ sub path_collapse
     my @p_parts=File::Spec->splitdir($path);
     my @outpath;
     my $indent="";
+    # Pretty sure split dir takes care of multislashes.
+    #my $multi_slash_count=0;
     foreach (@p_parts) {
 	if ($_ !~ /[.]{2}/ ) {#&& scalar(@outpath)>0 ){
-	    print($indent."$_ (add) \n") if ($debug_val>40);
-	    push(@outpath,$_);
-	    $indent=$indent."  " if ($debug_val>40);
-	} elsif(scalar(@outpath)==0 ){
+            
+            #if ($_ eq "" ){
+            #    $multi_slash_count++;
+            #} else {
+            #    $multi_slash_count=0;         
+            #}
+            #if ($multi_slash_count<=1){
+                print($indent."$_ (add) \n") if ($debug_val>40);
+                push(@outpath,$_);
+                $indent=$indent."  " if ($debug_val>40);
+            #} else {
+            #    printd(40,"Multi-slash reduction");
+            #}
+        } elsif(scalar(@outpath)==0 ){
 	    die("NOT ENOUGH PATH PARTS");
 	} else {
 	    #	    $indent=~s/  //;
@@ -277,17 +362,17 @@ sub lib_parse
     if ( ! -f $conf_inpath) { # check inpath for a lib.conf.
 	cluck( "No conf, not a lib");
     } else {
-	#if ( ! -f $conf_outpath ){
-	#}
+        # check output timestamp on lib.conf to see if we should update.
 	my $in_ts=mod_time($conf_inpath);
-	my $out_ts=$in_ts-1; # because out doesnt have to exist, we'll make a dummy case of just one less
+         # because out doesnt have to exist, we'll make a dummy case of just one less
+	my $out_ts=$in_ts-1;
 	if ( -e $conf_outpath){
 	    $out_ts= mod_time($conf_outpath);
 	}
-	if ( $in_ts > $out_ts ) { # if its newer, we'll want to transfer that.
+	if ( $in_ts > $out_ts ) {
 	    $work_list->{'lib.conf'}=$lib_template{'lib.conf'};
 	}
-
+        
 	my $lib_conf=load_lib_conf($conf_inpath);
 	my $lib_path=$lib_conf->get_value('Path');
 	##
@@ -303,24 +388,30 @@ sub lib_parse
 	    foreach (@conf_stack) {
 		my $nx_conf=load_lib_conf($_);
 		$nx_conf->delete_key('Path');
-		# this is the only key which may cause trouble.
+		# this is the only key which may cause trouble.in
 		# That is becuase it is not straight inherrited, on load it is followed,
 		# and then blanked any time its encountered.
 		$par_conf->copy_in($nx_conf);
 	    }
 	    $par_conf->copy_in($own_conf);
 	    $lib_conf=$par_conf;
-	    
-	    #$lib_conf->set_value("ParConf","$conf_stack[$#conf_stack]");
+            #$lib_conf->set_value("ParConf","$conf_stack[$#conf_stack]");
 	}
 	$lib_conf->print();
 	
 	my $lib_name=$lib_conf->get_value('LibName');
 	my $file_pat=$lib_conf->get_value('FilePattern');
 	my $test_bool=$lib_conf->get_value('TestingLib');#=true
+
+        # check if lib is a test version
 	if ($test_bool =~ /^([Tt][Rr][Uu][Ee]|1)$/x ) {
 	    #confess( "TEST STATUS($test_bool) TRUE");
-	    $test_bool=1;
+            $test_bool=1;
+            # jump back early if this is a testing lib, we want to ignore those.
+            # eg, we only want to let them copy their configs, but not their data.
+            carp("Testing lib! will not process sub components!");
+            sleep_with_countdown(1);
+            return $work_list;
 	} else {
 	    #confess( "TEST STATUS($test_bool) FALSE");
 	    $test_bool=0;
@@ -493,23 +584,25 @@ sub load_lib_conf
 
 sub do_work
 {
-    my ($inpath,$outpath,$work)=@_;
+    my ($in_dir,$out_dir,$work)=@_;
     my @fail_paths=();
 
     foreach (keys %$work ){
+        # each key of work is an item in in_dir
+        
 	my $cp_flags=$cmd_flags{$work->{$_}}; # work vall will be either dir|file
 	my ($cmd,$in_ts,$out_ts,$in_p,$out_p);
-	$out_p=$outpath."/$_";
+	$out_p=$out_dir."/$_";
 
 	## if not rm
 	if ( $work->{$_} !~ /rm/ ){
-	    $in_p=$inpath."/$_";
+	    $in_p=$in_dir."/$_";
 	    confess("IN_P:$in_p missing!") if ( ! -e $in_p );
 	    $in_ts=mod_time("$in_p");
-	    $in_p="\'".$inpath."/$_"."\'";
+	    $in_p="\'".$in_dir."/$_"."\'";
 	    $out_ts=$in_ts-1;
 	} else {
-	    # rm mode, no inpath. no in time.
+	    # rm mode, no in_dir. no in time.
 	    $in_p="";
 	    $out_ts=mod_time("$out_p");
 	    $in_ts=$out_ts-1;
@@ -529,6 +622,7 @@ sub do_work
 	    #print("\t$update_cmd{$work->{$_}} $cp_flags $in_p $outp\n");
 	#}
 	my $update=$update_cmd{$work->{$_}};
+        # libconfs string
 	my $lc="";
 	#push(@{$lib_stacks->{$lib_path}},$conf_inpath);
 	if ( $work->{$_} =~ /lib/ ) {
@@ -543,6 +637,8 @@ sub do_work
 		#if ($can_dump){
 		#    Data::Dump::dump("Lib $_ libstack not defined.");}
 	    }
+            $in_p="\'".$in_dir."\'";
+            $outp="\'".$out_dir."\' \'$_\'";
 	}
 
 	$cmd= "$update $lc $cp_flags $in_p $outp";
@@ -551,7 +647,6 @@ sub do_work
 #	}
 
 	my $fail_cond=0;
-
 	###
 	# open and readline print with tab at front.
 	###
@@ -562,14 +657,17 @@ sub do_work
 	    print("Debugging over 50, $debug_val\n");
 	    $cmd="echo ".$cmd;
 	}
+
+        # Effecively, run_and_watch except there is an auto indent of output.
+        # testing, run_and_watch($cmd,"\t");
 	my $pid = open( my $CID,"-|", "$cmd"  ) ;
-	
-	print("PID:$pid\n") if $debug_val>=45;
+        print("PID:$pid\n") if $debug_val>=45;
 	while ( my $line=<$CID> ) {
 	    $line =~ s/\s+$//;
 	    print("\t".$line."\n");
 	}
-	if ( $work->{$_} =~ /rm/ ) {
+
+        if ( $work->{$_} =~ /rm/ ) {
 	    if ( -e "$out_p") {
 		print("Remaining file error:\n\t$cmd\n -e $out_p\n");
 		$fail_cond++;
